84772a6f2af697d27f0dafc9925b5bb0af118c4f
Sergey Radchenko <ssr14@tpu.ru>
2024-11-02 16:15:43 +0700

    small fixes

--- src/main/java/com/example/taskmanagementsystem/repo/UserRepository.java
+++ src/main/java/com/example/taskmanagementsystem/repo/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.taskmanagementsystem.repo;
 import com.example.taskmanagementsystem.domain.models.jpa.UserEntity;
 import com.example.taskmanagementsystem.enums.UserRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     boolean existsByEmail(String email);
     Optional<UserEntity> getByEmail(String email);
+    @Query("SELECT COUNT(*) FROM UserEntity")
     long countAll();
 }
